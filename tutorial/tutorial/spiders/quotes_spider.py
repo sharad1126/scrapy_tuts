38123d7f8b9e37c6dc5f1c78711e0b5fa28f171e
sharad1126 <sharad.agarwal2014@vit.ac.in>
2018-05-16 10:53:26 +0200

    scrapy doesn't works for js pages

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -6,8 +6,7 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'http://hp22f13a.cern.ch/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
